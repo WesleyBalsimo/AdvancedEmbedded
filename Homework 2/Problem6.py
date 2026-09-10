a62d85930c46ba75343c36aee59fe0df2a74dac2
WesleyBalsimo <wbalsimo@icloud.com>
2026-09-09 19:21:05 -0500

    do problem 6

---
+++ Homework 2/Problem6.py
@@ -0,0 +1,12 @@
+#Sequentially turn on leds on gp16-18
+
+from machine import Pin
+from time import sleep
+
+led = [Pin(i, Pin.OUT) for i in range(16, 19)]
+
+while True:
+    for i in range (3):
+        led[i].on()
+        sleep(1.0)
+        led[i].off()
